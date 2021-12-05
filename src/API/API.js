a3db9fb35fc5d991e5aacf7e47bfa036decd20ea
Danish Raza <danishraza0333@gmail.com>
2021-12-05 16:10:35 +0500

    Add API methods

--- src/API/API.js
+++ src/API/API.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
 const url = 'http://localhost:3000/users';
-const getStudents = async () => {
-  return await axios.get(url);
+
+export const getStudents = async (id) => {
+  id = id || '';
+  return await axios.get(`${url}/${id}`);
 };
 
-export default getStudents;
+export const addStudent = async (student) => {
+  return await axios.post(url, student);
+};
+
+export const editStudent = async (student, id) => {
+  return await axios.put(`${url}/${id}`, student);
+};
+
+export const deleteStudent = async (id) => {
+  return await axios.delete(`${url}/${id}`);
+};
